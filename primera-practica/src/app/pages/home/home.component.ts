c7e348d11d776cdcb1cb43888a2cd2807020ce1c
Pablo Iglesias <piglesiass03@iesalbarregas.es>
2026-05-06 11:54:47 +0200

    implementacion-footer

--- primera-practica/src/app/pages/home/home.component.ts
+++ primera-practica/src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CardComponent],
+  imports: [CardComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
