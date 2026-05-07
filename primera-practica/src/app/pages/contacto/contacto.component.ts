84c27b0549e6ee77e9af78891deffa9632aede5e
Pablo Iglesias <piglesiass03@iesalbarregas.es>
2026-05-07 12:57:08 +0200

    más estilos

--- primera-practica/src/app/pages/contacto/contacto.component.ts
+++ primera-practica/src/app/pages/contacto/contacto.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { TranslateModule } from '@ngx-translate/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+
 
 @Component({
   selector: 'app-contacto',
-  imports: [FooterComponent, TranslateModule],
+  imports: [FooterComponent, TranslateModule, MatInputModule, MatFormFieldModule, FormsModule, MatCardModule],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.scss'
 })
--- primera-practica/src/app/pages/user-detalles/user-detalles.component.ts
+++ primera-practica/src/app/pages/user-detalles/user-detalles.component.ts
@@ -1,10 +1,10 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService, User } from '../../services/user.service';
-
+import {MatCardModule} from '@angular/material/card';
 @Component({
   selector: 'app-user-detalles',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './user-detalles.component.html',
   styleUrl: './user-detalles.component.scss'
 })
